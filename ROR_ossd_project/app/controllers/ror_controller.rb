17ee43fed4c01a7a6f80e71e706d04d1679caab8
Hamzaattiq <13022020048@umt.edu.pk>
2015-05-06 22:45:12 +0500

    ROR Project created

---
+++ ROR_ossd_project/app/controllers/ror_controller.rb
@@ -0,0 +1,52 @@
+class RorController < ApplicationController
+  def Blackberry
+  end
+
+  def Gfive
+  end
+
+  def Home
+  end
+
+  def HTC
+  end
+
+  def Huawei
+  end
+
+  def mainpg
+  end
+
+  def iphone
+  end
+
+  def Lenovo
+  end
+
+  def microsoft
+  end
+
+  def Motorola
+  end
+
+  def Nokia
+  end
+
+  def OPPO
+  end
+
+  def Qmobile
+  end
+
+  def Rivo
+  end
+
+  def Samsung
+  end
+
+  def Sony
+  end
+
+  def Voice
+  end
+end
---
+++ ROR_ossd_project/test/controllers/ror_controller_test.rb
@@ -0,0 +1,89 @@
+require 'test_helper'
+
+class RorControllerTest < ActionController::TestCase
+  test "should get Blackberry" do
+    get :Blackberry
+    assert_response :success
+  end
+
+  test "should get Gfive" do
+    get :Gfive
+    assert_response :success
+  end
+
+  test "should get Home" do
+    get :Home
+    assert_response :success
+  end
+
+  test "should get HTC" do
+    get :HTC
+    assert_response :success
+  end
+
+  test "should get Huawei" do
+    get :Huawei
+    assert_response :success
+  end
+
+  test "should get mainpg" do
+    get :mainpg
+    assert_response :success
+  end
+
+  test "should get iphone" do
+    get :iphone
+    assert_response :success
+  end
+
+  test "should get Lenovo" do
+    get :Lenovo
+    assert_response :success
+  end
+
+  test "should get microsoft" do
+    get :microsoft
+    assert_response :success
+  end
+
+  test "should get Motorola" do
+    get :Motorola
+    assert_response :success
+  end
+
+  test "should get Nokia" do
+    get :Nokia
+    assert_response :success
+  end
+
+  test "should get OPPO" do
+    get :OPPO
+    assert_response :success
+  end
+
+  test "should get Qmobile" do
+    get :Qmobile
+    assert_response :success
+  end
+
+  test "should get Rivo" do
+    get :Rivo
+    assert_response :success
+  end
+
+  test "should get Samsung" do
+    get :Samsung
+    assert_response :success
+  end
+
+  test "should get Sony" do
+    get :Sony
+    assert_response :success
+  end
+
+  test "should get Voice" do
+    get :Voice
+    assert_response :success
+  end
+
+end
